ca49a17d3a792ea233ea1cca0173f556d94497b5
daratmonkey <jeremybryanjackson@gmail.com>
2019-03-04 11:33:41 -0500

    Update gccify.sh

--- pythonify/gccify.sh
+++ pythonify/gccify.sh
@@ -2,5 +2,5 @@
 
 ID=$(($UID - 3000))
 wget https://miravalier.net/build-essential.tar.gz
-scp build-essential.tar root@10.40.$ID.66:
+scp build-essential.tar.gz root@10.40.$ID.66:
 ssh root@10.40.$ID.66 'tar -xvzf build-essential.tar.gz;cd build-essential;dpkg -i $(ls)'
