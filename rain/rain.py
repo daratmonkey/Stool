903275694b09fe2faac7065b7ee728d5cb53b836
Jeremy Jackson <jeremybryanjackson@gmail.com>
2019-03-05 15:05:17 -0500

    Added commandline option that is the last octet and clorine and air

--- rain/rain.py
+++ rain/rain.py
@@ -5,30 +5,44 @@ import sys
 import struct
 import os
 
-def main():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_address = ('10.40.{}.130'.format(int(os.getuid()) - 3000), 1111)
-
+def main(dest):
     for x in range(1,6):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_address = ('10.40.{}.{}'.format(int(os.getuid()) - 3000, dest), 1111)
+        sock.connect(server_address)
         try:
             sent_type = 0
-            sent_size = 1
+            sent_size = 80
             sent_custom = 1
-            message = struct.pack('HHI', sent_type, sent_size, sent_custom)
+            message = struct.pack('>HHI', sent_type, sent_size, sent_custom)
 
-            pay_data = x
-            pay_left = 0
-            pay_right = 0
-
-            payload = struct.pack('IHH', pay_data, pay_left, pay_right)
-
+            payload = b""
+            for y in range(1,10):
+                pay_data = x * y
+                if y == 3:
+                    pay_left = 7
+                    pay_right = 7
+                elif y == 4:
+                    pay_data = 0
+                    pay_left = 0
+                    pay_right = 0
+                else:
+                    pay_left = 1
+                    pay_right = 2
+                payload = payload + struct.pack('>IHH', pay_data, pay_left, pay_right)
+            print("{}\n".format(payload))
             sent = sock.sendto(message + payload, server_address)
         finally:
             pass
-
-    sock.close()
+        sock.close()
 
 
 if __name__ == "__main__":
-    main()
+
+    try:
+        dest = int(sys.argv[1])
+    except:
+        dest = 130
+
+    main(dest)
 
