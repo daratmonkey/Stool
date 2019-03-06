f385f9a88ebbaabe88d2349b0451c83cd1b25f66
jjackson <jjackson@none>
2019-03-06 13:35:13 -0500

    Fixed a typo

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ if [ "$#" -eq 0 ]; then
             scp $file root@10.40.$ID.$box:~
         done
     done
-elif [ "$#" -eq 0 ]; then
+elif [ "$#" -eq 1 ]; then
     for box in 130 235 1 66; do
         scp $1 root@10.40.$ID.$box:~
     done
