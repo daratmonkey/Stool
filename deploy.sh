5524854c5ca17b6074cb946bbb89314e3365e140
Jeremy Jackson <jeremybryanjackson@gmail.com>
2019-03-06 13:33:05 -0500

    updated deploy to send one file

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,18 @@
 
 ID=$(($UID - 3000))
 
-for box in 130 235 1 66; do
-    for file in $(ls); do
-        scp $file root@10.40.$ID.$box:~
+if [ "$#" -eq 0 ]; then
+    for box in 130 235 1 66; do
+        for file in $(ls); do
+            scp $file root@10.40.$ID.$box:~
+        done
     done
-done
+elif [ "$#" -eq 0 ]; then
+    for box in 130 235 1 66; do
+        scp $1 root@10.40.$ID.$box:~
+    done
+else
+    echo "Unknown options"
+fi
+
+
