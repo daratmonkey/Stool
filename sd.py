1c6810e50cf5963c0bc50a708c036732ff768465
Jeremy Jackson <jeremybryanjackson@gmail.com>
2019-03-06 13:24:10 -0500

    application

--- sd.py
+++ sd.py
@@ -44,11 +44,39 @@ while inputs:
                             the_mix.append(molecule)    
                     except:
                         print("Could not unpack\n")
+                    
+                    debris = []
+
+                    for x in the_mix:
+                        if x[1] > len(the_mix) or x[2] > len(the_mix):
+                            print("->{} {} {}<-".format(x, x[1], x[2]))
+                            debris.append(x)
+                            the_mix.remove(x)
+
+                    debris_final = struct.pack('!HHI', 1, (len(debris) * 8) + 8, 0)
+                    for x in debris:
+                        debris_final += struct.pack('!IHH', x[0], 0, 0) 
+
+                    if len(debris) > 0:
+                        debris_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                        debris_adress = ('10.40.13.151', 2222)
+                        debris_socket.connect(debris_adress)
+                        debris_socket.send(debris_final)
+                        debris_socket.close()
+                        fp.write("DEBR: {}\n".format(debris_final))
+                        print("DEBR: {}\n".format(debris_final))
+
+                    water_final = struct.pack('!HHI', 0, (len(the_mix) * 8) + 8, 0)
+                    for y in the_mix:
+                        water_final += struct.pack('!IHH', y[0], y[1], y[2])
+
                     ds_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                     ds_address = ('10.40.13.151', 1111)
                     ds_socket.connect(ds_address)
-                    ds_socket.send(data)
+                    ds_socket.send(water_final)
+                    print("SEND: {}\n".format(water_final))
                     ds_socket.close()
+
                     print("DATA: [{}] {}\n".format(len(the_mix), the_mix))
                     fp.write("DATA: [{}] {}\n".format(len(the_mix), the_mix))
                     clorine = 0
